d8263cb3453c8b882906bc940b8413653218be82
Raihan Aria <raihan.aria.ft21@mail.umy.ac.id>
2022-12-08 15:46:26 +0700

    create class stack

--- exe4_016/Program.cs
+++ exe4_016/Program.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace exe4_016
 {
-
+    class Stack
+    {
+        private String[] ar = new string[37];
+        private int top;
+        private int max;
+        public Stack()
+        {
+            top = -1;
+            max = ar.Length;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
